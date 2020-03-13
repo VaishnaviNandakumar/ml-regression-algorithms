f74f3c0c034a6b55b76bc6cf1d496863c8060ee9
Vaishnavi Nandakumar <vaishoo.nk@gmail.com>
2020-03-14 00:48:07 +0530

    Visualized Dataset for Polynomial Regression

--- polynomial_linear_regression.py
+++ polynomial_linear_regression.py
@@ -20,7 +20,31 @@ lin_reg.fit(X, y)
 
 #Fiting Polynomial Regression to Dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg =  PolynomialFeatures(degree = 2)
+poly_reg =  PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X)
 lin_reg2 = LinearRegression()
 lin_reg2.fit(X_poly, y)
+
+#Visualizing Linear Regression Result
+plt.scatter(X, y , color ='red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Results for Linear Regression')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualizing Polynomial Regression Result
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X, y , color ='red')
+plt.plot(X_grid, lin_reg2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Results for Polynomial Regression')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+#Prediction Using Linear Regression
+lin_reg.predict([[6.5]])
+#Prediction Using Polynomial Regression
+lin_reg2.predict(poly_reg.fit_transform([[6.5]]))
+
