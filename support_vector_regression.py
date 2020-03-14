c8b24901b4d3eb70a70f5868ffeba3a17b4f3c4b
Vaishnavi Nandakumar <vaishoo.nk@gmail.com>
2020-03-14 20:03:12 +0530

    Support Vector Regression

---
+++ support_vector_regression.py
@@ -0,0 +1,34 @@
+#Support Vector Regression
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+"""# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+
+#Fitting Linear Regression to Dataset
+from sklearn.linear_model import LinearRegression
+lin_reg =  LinearRegression()
+lin_reg.fit(X, y)
+
+#Fiting Polynomial Regression to Dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg =  PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(X_poly, y)
+
+#Visualizing Linear Regression Result
+plt.scatter(X, y , color ='red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Results for Linear Regression')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
