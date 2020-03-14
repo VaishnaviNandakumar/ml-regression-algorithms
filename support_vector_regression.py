843dfab4369589dcbaf1b6dc805c8d2200cb957e
Vaishnavi Nandakumar <vaishoo.nk@gmail.com>
2020-03-14 22:31:15 +0530

    Prediction SVR

--- support_vector_regression.py
+++ support_vector_regression.py
@@ -8,27 +8,25 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
-"""# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+# Feature Scaling (Required here cause SVR is a less common module)
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
 
+#Fitting SVR to Dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X,y)
 
-#Fitting Linear Regression to Dataset
-from sklearn.linear_model import LinearRegression
-lin_reg =  LinearRegression()
-lin_reg.fit(X, y)
-
-#Fiting Polynomial Regression to Dataset
-from sklearn.preprocessing import PolynomialFeatures
-poly_reg =  PolynomialFeatures(degree = 4)
-X_poly = poly_reg.fit_transform(X)
-lin_reg2 = LinearRegression()
-lin_reg2.fit(X_poly, y)
+#Predicting results
+y_pred = regressor.predict([[6.5]])
 
 #Visualizing Linear Regression Result
 plt.scatter(X, y , color ='red')
-plt.plot(X, lin_reg.predict(X), color = 'blue')
-plt.title('Results for Linear Regression')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Results for SVR')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
